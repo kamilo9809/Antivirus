0baa69ebc4a3224c3f4f8706988cb91816230f82
kamilo9809 <camilorivera09@outlook.com>
2024-11-16 12:33:32 -0500

    fix: :bug: facebook and google authlogin

--- src/app/components/pages/login/loginComponents/social-login/social-login.component.ts
+++ src/app/components/pages/login/loginComponents/social-login/social-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthSocialService } from '../../services/auth-social.service';
 
 @Component({
   selector: 'app-social-login',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./social-login.component.css']
 })
 export class SocialLoginComponent {
-
+  constructor(
+    private authSocialMediaService: AuthSocialService
+){}
+signInWithGoogle() {
+  this.authSocialMediaService
+    .signInWithGoogle()
+    .then((result) => {
+      console.log('User signed in:', result.user);
+    })
+    .catch((error) => {
+      console.error('Error signing in with Google:', error);
+      });
+  }
+loginWithFacebook() {
+  this.authSocialMediaService.signInWithFacebook()
+    .then((result) => {
+      console.log('Logged in with Facebook:', result);
+    })
+    .catch((error) => {
+      console.error('Error logging in with Facebook:', error);
+      });
+  }
 }
